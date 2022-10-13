033bbd46ea12eb9f87bf9c5ab9c3c5064abfd6f3
jaryarbn <grandline501@gmail.com>
2022-10-13 12:58:20 +0800

    Bookshop

---
+++ src/main/java/com/example/demo/BookShopBean.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
+
+public class BookShopBean {
+    private String submit;
+    private String item;
+    ArrayList<String> arrayList = new ArrayList<>();
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getSubmit() {
+        return submit;
+    }
+
+    public void setSubmit(String submit) {
+        this.submit = submit;
+    }
+
+    private void addItem(String item) {
+        arrayList.add(item);
+    }
+
+    public ArrayList<String> getItems() {
+
+        return arrayList;
+    }
+
+    public void processRequest(HttpServletRequest request) {
+        if (submit == null) {
+            addItem(item);
+        }
+        if (submit.equals("¹ºÂò")) {
+            addItem(item);
+        } else {
+            reset();
+        }
+    }
+    private void reset() {
+        setSubmit(null);
+        setItem(null);
+    }
+}
